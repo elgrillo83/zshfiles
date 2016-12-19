66beb8b47cf3a23e6fb1c1288373eeacafa255b0
elgrillo83 <elgrillo83@gmail.com>
2016-12-19 13:19:00 +0100

    Link custom .zshrc and source again

--- setup.sh
+++ setup.sh
@@ -5,3 +5,12 @@ brew install zsh
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Remove generated .zshrc
+rm $HOME/.zshrc
+
+# Link custom .zshrc
+ln -s $PWD/zshrc $HOME/.zshrc
+
+# Source newly linked .zshrc
+source $HOME/.zshrc
